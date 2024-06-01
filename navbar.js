e4b9d834577ee584a229981852d8dd6d87632296
Marius Odediran <diranmarius8@gmail.com>
2024-06-01 15:47:06 +0100

    Doen with navbar

--- navbar.js
+++ navbar.js
@@ -1,5 +1,7 @@
 const navBar = document.querySelector('.hamburger-menu');
+const navItems = document.querySelector('.nav-items');
 
 navBar.addEventListener('click', () => {
   navBar.classList.toggle('active');
+  navItems.classList.toggle('active');
 })
