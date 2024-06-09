4a97e0003524a3b1919156660eb1044592fa081e
Marius Odediran <diranmarius8@gmail.com>
2024-06-09 08:14:50 +0100

    added new codes

--- home.js
+++ home.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".auto-type", {
-  strings : ['Front-End Developer.', "UI/UX Designer.", "Cloud-Engineer", "#Genztechie."],
+  strings : ['Front-End Developer.', "UI/UX Designer.", "Cloud-Engineer.", "#Genztechie."],
   typeSpeed : 150,
   backSpeed : 100,
   loop : true,
